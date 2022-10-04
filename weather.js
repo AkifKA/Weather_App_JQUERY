7416f9fdfea9d6586effaafb17f5bc185b808289
F4250-Akif KA <karaozmehmetakif@gmail.com>
2022-10-04 20:49:50 +0300

    2

--- weather.js
+++ weather.js
@@ -1,4 +1,4 @@
-const formJS = document.querySelectorAll("form");
+// const formJS = document.querySelectorAll("form")[0];
 // const formJquery = jQuery("form");
 const formJquery = $("form").eq(0);
 const inputJQ = $(".top-banner input").eq(0);
@@ -11,3 +11,15 @@ const list = $(".cities").eq(0);
 // get(index) ==> toArray(get()), eq(index)
 
 // load VS DOMContentLoaded
+
+//window.onload = () => {}; ==> JS
+
+$(window).on("load", () => {
+  console.log("window.load");
+});
+
+// document.addEventListener("DOMContentLoaded", () => {});
+
+$(document).ready(() => {
+  console.log("DOMContentLoaded");
+});
