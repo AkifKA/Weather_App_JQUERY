98877a68e8579f4e8999c866f7d43091335e1119
F4250-Akif KA <karaozmehmetakif@gmail.com>
2022-10-04 20:36:47 +0300

    proje başladı

---
+++ weather.js
@@ -0,0 +1,9 @@
+const formJS = document.querySelectorAll("form");
+// const formJquery = jQuery("form");
+const formJquery = $("form").eq(0);
+const inputJQ = $(".top-banner input").eq(0);
+const msgJQ = $(".top-banner span").eq(0);
+const list = $(".cities").eq(0);
+
+// console.log(formJS);
+console.log(inputJQ);
